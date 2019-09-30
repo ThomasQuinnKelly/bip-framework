442fa638b0c5b8fac0f5eaf25ff86eed32394644
VHAISAKulkaA <VHAISAKulkaA@ISA-LT85VMTC2.vha.med.va.gov>
2019-09-30 11:41:46 -0400

    AF-525# Added request body to the PUT and DELETE methods
    AF-524# Fixed the milliseconds to seconds incorrect logging part

--- bip-framework-libraries/src/main/java/gov/va/bip/framework/aspect/PerformanceLoggingAspect.java
+++ bip-framework-libraries/src/main/java/gov/va/bip/framework/aspect/PerformanceLoggingAspect.java
@@ -9,14 +9,14 @@ import gov.va.bip.framework.log.BipLogger;
 import gov.va.bip.framework.log.BipLoggerFactory;
 
 /**
- * Performance logging (elapsed execution time).
- * This class should only be invoked from {@code @Aspect} annotated classes,
- * and only from {@code @Around} advice.
+ * Performance logging (elapsed execution time). This class should only be
+ * invoked from {@code @Aspect} annotated classes, and only from {@code @Around}
+ * advice.
  *
- * Developers note: this class cannot be converted to use {@code @Before}
- * and {@code @After} advice. It would be necessary to maintain state between
- * the advice calls for the startMillis value. Spring can only maintain
- * threadsafety if injected (state) values are proxied, and a Long is not proxied.
+ * Developers note: this class cannot be converted to use {@code @Before} and
+ * {@code @After} advice. It would be necessary to maintain state between the
+ * advice calls for the startMillis value. Spring can only maintain threadsafety
+ * if injected (state) values are proxied, and a Long is not proxied.
  *
  * @author aburkholder
  */
@@ -25,7 +25,7 @@ public class PerformanceLoggingAspect {
 	private static final BipLogger LOGGER = BipLoggerFactory.getLogger(PerformanceLoggingAspect.class);
 
 	/** number of milliseconds in a second */
-	private static final int NUMBER_OF_MILLIS_N_A_SECOND = 1000;
+	private static final double NUMBER_OF_MILLIS_N_A_SECOND = 1000.0;
 
 	/** The Constant IN_ELAPSED_TIME. */
 	private static final String IN_ELAPSED_TIME = "] in elapsed time [";
@@ -57,13 +57,14 @@ public class PerformanceLoggingAspect {
 
 	/**
 	 *
-	 * This method should only be invoked from {@code @Aspect} annotated classes,
-	 * and only from {@code @Around} advice.
+	 * This method should only be invoked from {@code @Aspect} annotated
+	 * classes, and only from {@code @Around} advice.
 	 *
 	 * Developers note: this class cannot be converted to use {@code @Before}
-	 * and {@code @After} advice. It would be necessary to maintain state between
-	 * the advice calls for the startMillis value. Spring can only maintain
-	 * threadsafety if injected (state) values are proxied, and a Long is not proxied.
+	 * and {@code @After} advice. It would be necessary to maintain state
+	 * between the advice calls for the startMillis value. Spring can only
+	 * maintain threadsafety if injected (state) values are proxied, and a Long
+	 * is not proxied.
 	 *
 	 * @param joinPoint
 	 * @return
@@ -93,13 +94,11 @@ public class PerformanceLoggingAspect {
 		} finally {
 			LOGGER.debug("PerformanceLoggingAspect after method was called.");
 			final long elapsedTime = System.currentTimeMillis() - startTime;
-			final String callingClassAndMethod =
-					method == null ? "null"
-							: method.getDeclaringClass().getSimpleName() + DOT + method.getName();
+			final String callingClassAndMethod = method == null ? "null"
+					: method.getDeclaringClass().getSimpleName() + DOT + method.getName();
 			if (methodLog != null && methodLog.isInfoEnabled()) {
 				methodLog.info(EXIT + OPEN_BRACKET + callingClassAndMethod + IN_ELAPSED_TIME
-						+ elapsedTime / NUMBER_OF_MILLIS_N_A_SECOND
-						+ DOT + elapsedTime % NUMBER_OF_MILLIS_N_A_SECOND + SECS + CLOSE_BRACKET);
+						+ elapsedTime / NUMBER_OF_MILLIS_N_A_SECOND + SECS + CLOSE_BRACKET);
 			}
 		}
 		return returnObject;
