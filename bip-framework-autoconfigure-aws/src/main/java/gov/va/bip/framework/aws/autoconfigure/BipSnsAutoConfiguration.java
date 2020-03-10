6f3258c9a7e16b70ca5a72e7f8b37166193ec935
Michael Brzezinski <Brzezinski_Michael@Bah.com>
2020-03-10 16:14:09 -0400

    BipSnsAutoConfiguration and BipSqsAutoConfiguration enabled by bip.framework fix

--- bip-framework-autoconfigure-aws/src/main/java/gov/va/bip/framework/aws/autoconfigure/BipSnsAutoConfiguration.java
+++ bip-framework-autoconfigure-aws/src/main/java/gov/va/bip/framework/aws/autoconfigure/BipSnsAutoConfiguration.java
@@ -6,6 +6,7 @@ import gov.va.bip.framework.sns.config.StandardSnsConfiguration;
 import gov.va.bip.framework.sns.services.SnsService;
 import gov.va.bip.framework.sns.services.impl.SnsServiceImpl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableConfigurationProperties(SnsProperties.class)
+@ConditionalOnProperty(name = "bip.framework.localstack.services.sns.enabled", havingValue = "true")
 @Import({ AbstractSnsConfiguration.class, StandardSnsConfiguration.class })
 public class BipSnsAutoConfiguration {
 
