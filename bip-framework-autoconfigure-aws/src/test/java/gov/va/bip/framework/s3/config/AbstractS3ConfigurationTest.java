5c6036f2c0836fd91bbd5ee8efae5d256fea20a6
Taylor-Jackwood <jackwood_taylor@bah.com>
2020-03-13 13:20:29 -0400

    Fortify issue fix

--- bip-framework-autoconfigure-aws/src/test/java/gov/va/bip/framework/s3/config/AbstractS3ConfigurationTest.java
+++ bip-framework-autoconfigure-aws/src/test/java/gov/va/bip/framework/s3/config/AbstractS3ConfigurationTest.java
@@ -2,7 +2,6 @@ package gov.va.bip.framework.s3.config;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Region;
-import gov.va.bip.framework.config.BipCommonSpringProfiles;
 import org.junit.Test;
 import org.springframework.core.env.Environment;
 import org.springframework.mock.env.MockEnvironment;
@@ -19,46 +18,6 @@ public class AbstractS3ConfigurationTest {
 
 		final AbstractS3Configuration instance = new AbstractS3ConfigurationImpl();
 		final Environment env = new MockEnvironment();
-
-		final AmazonS3 amazonS3 = instance.amazonS3(s3Properties, env);
-		assertNotNull(amazonS3);
-	}
-
-	@Test
-	public void testCreateAmazonS3_2() {
-		final S3Properties s3Properties = new S3Properties();
-		s3Properties.setEndpoint("http://localhost:8080/endpoint");
-		s3Properties.setRegion(Region.US_West.toString());
-
-		final AbstractS3Configuration instance = new AbstractS3ConfigurationImpl();
-		final Environment env = new MockEnvironment() {
-			@Override
-			public String[] getActiveProfiles() {
-				String[] activeProfiles = new String[1];
-				activeProfiles[0] = BipCommonSpringProfiles.PROFILE_EMBEDDED_AWS;
-				return activeProfiles;
-			}
-		};
-
-		final AmazonS3 amazonS3 = instance.amazonS3(s3Properties, env);
-		assertNotNull(amazonS3);
-	}
-
-	@Test
-	public void testCreateAmazonS3_3() {
-		final S3Properties s3Properties = new S3Properties();
-		s3Properties.setEndpoint("http://localhost:8080/endpoint");
-		s3Properties.setRegion(Region.US_West.toString());
-
-		final AbstractS3Configuration instance = new AbstractS3ConfigurationImpl();
-		final Environment env = new MockEnvironment() {
-			@Override
-			public String[] getActiveProfiles() {
-				String[] activeProfiles = new String[1];
-				activeProfiles[0] = BipCommonSpringProfiles.PROFILE_ENV_LOCAL_INT;
-				return activeProfiles;
-			}
-		};
 
 		final AmazonS3 amazonS3 = instance.amazonS3(s3Properties, env);
 		assertNotNull(amazonS3);
